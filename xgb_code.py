ff6e4abdd83598e3bb8d8ebfd5d887612c722755
Bryan <bryanching7373@gmail.com>
2018-05-09 21:32:06 -0700

    booooooost

--- xgb_code.py
+++ xgb_code.py
@@ -13,11 +13,17 @@ def boost():
     dtest = xgb.DMatrix(testNP_X)
 
     #evallist = [(dtest, 'eval'), (dtrain, 'train')]
-    num_round = 10
-    param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':len(YDict) }
-    booster = xgb.train(param, dtrain, num_round)
+    num_round = 999
+    param = {'max_depth':10, 'eta':0.3, 'silent':1, 'objective':'multi:softmax', 'num_class':len(YDict)}
+    #classifier = xgb.train(param, dtrain, num_round)
 
-    categories = booster.predict(dtest)
-    print(categories)
+    cv = xgb.cv(param
+                , dtrain
+                , num_boost_round = num_round
+                , nfold = 4
+                , early_stopping_rounds = 10)
+    print(cv)
+
+    categories = classifier.predict(dtest)
 
     return categories, YDict
